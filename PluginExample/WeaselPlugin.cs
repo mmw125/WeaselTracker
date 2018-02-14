29d176624ec401689956f4875b139404241a0331
Mark Wiggans <mmw125@gmail.com>
2018-02-14 13:46:28 -0500

    initial commit

---
+++ PluginExample/WeaselPlugin.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Controls;
+using Hearthstone_Deck_Tracker.API;
+using Hearthstone_Deck_Tracker.Plugins;
+
+namespace Weasel
+{
+	public class WeaselPlugin : IPlugin
+	{
+		private Display _display;
+
+		public string Author
+		{
+			get { return "mmw125"; }
+		}
+
+		public string ButtonText
+		{
+			get { return "Settings"; }
+		}
+
+		public string Description
+		{
+			get { return "Displays the number of weasels in opponnent's deck and hand."; }
+		}
+
+		public MenuItem MenuItem
+		{
+			get { return null; }
+		}
+
+		public string Name
+		{
+			get { return "Weasel Tracker"; }
+		}
+
+		public void OnButtonPress()
+		{
+		}
+
+		public void OnLoad()
+		{
+			_display = new Display();
+			Core.OverlayCanvas.Children.Add(_display);
+			WeaselTracker tracker = new WeaselTracker(_display);
+
+            GameEvents.OnGameStart.Add(tracker.GameStart);
+            GameEvents.OnGameEnd.Add(tracker.GameEnd);
+            GameEvents.OnPlayerCreateInPlay.Add(tracker.OnCreateInPlay);
+            GameEvents.OnOpponentCreateInPlay.Add(tracker.OnCreateInPlay);
+            GameEvents.OnPlayerPlay.Add(tracker.OnCreateInPlay);
+            GameEvents.OnOpponentPlay.Add(tracker.OnCreateInPlay);
+        }
+
+		public void OnUnload()
+		{
+			Core.OverlayCanvas.Children.Remove(_display);
+		}
+
+		public void OnUpdate()
+		{
+		}
+
+		public Version Version
+		{
+			get { return new Version(0, 1, 1); }
+		}
+	}
+}
---
+++ PluginExample/WeaselTracker.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HearthDb.Enums;
+using Hearthstone_Deck_Tracker;
+using Hearthstone_Deck_Tracker.API;
+using Hearthstone_Deck_Tracker.Enums;
+using Hearthstone_Deck_Tracker.Hearthstone;
+using Hearthstone_Deck_Tracker.Hearthstone.Entities;
+using CoreAPI = Hearthstone_Deck_Tracker.API.Core;
+
+namespace Weasel
+{
+	internal class WeaselTracker
+	{
+        private Display _display;
+        private int weasels = 0;
+
+		public WeaselTracker(Display display)
+		{
+            _display = display;
+			// Hide in menu, if necessary
+			if (Config.Instance.HideInMenu && CoreAPI.Game.IsInMenu)
+				_display.Hide();
+		}
+
+		// Reset on when a new game starts
+		internal void GameStart()
+		{
+            _display.UpdateNumber(0);
+            _display.Hide();
+		}
+
+        // Reset on when a new game starts
+        internal void GameEnd()
+        {
+            _display.UpdateNumber(0);
+            _display.Hide();
+        }
+
+        // Need to handle hiding the element when in the game menu
+        internal void InMenu()
+		{
+			if (Config.Instance.HideInMenu)
+			{
+				_display.Hide();
+			}
+		}
+
+        internal void OnCreateInPlay(Card card)
+        {
+            if (card.Id == "CFM_095")
+            {
+                weasels += 1;
+                _display.UpdateNumber(weasels);
+                _display.Show();
+            }
+        }
+	}
+}
